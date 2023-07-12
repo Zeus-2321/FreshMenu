1a1eb2321a58eb680ff4f3dd6872d50bfce9d193
Dev <devpratap2321@gmail.com>
2023-07-12 21:22:42 +0530

    added dynamic routes

--- practice/app/details/[slug]/page.tsx
+++ practice/app/details/[slug]/page.tsx
@@ -1,44 +1,44 @@
-// "use client"
-// import React, { useState, useEffect } from 'react';
-// import axios from 'axios';
+"use client"
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-// interface DetailsProps {
-//   slug: string;
-// }
+interface DetailsProps {
+  slug: string;
+}
 
-// const Details: React.FC<DetailsProps> = ({ slug }) => {
-//   const [phone, setPhone] = useState(null);
+const Details: React.FC<DetailsProps> = ({ slug }) => {
+  const [phone, setPhone] = useState(null);
 
-//   useEffect(() => {
-//     console.log(slug)
-//     fetchData();
-//   }, []);
+  useEffect(() => {
+    console.log(slug)
+    fetchData();
+  }, []);
 
-//   const fetchData = async () => {
-//     try {
-//       const response = await axios.get(`https://phone-specs-api.vercel.app/${slug}`);
-//       setPhone(response.data.data);
-//     } catch (error) {
-//       console.error(error);
-//     }
-//   };
+  const fetchData = async () => {
+    try {
+      const response = await axios.get(`https://phone-specs-api.vercel.app/${slug}`);
+      setPhone(response.data.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
-//   return (
-//     <div className="details-container">
-//       <div className="details-header">
-//         <h2>{phone.brand} {phone.phone_name}</h2>
-//         {/* Rest of the code */}
-//       </div>
+  return (
+    <div className="details-container">
+      <div className="details-header">
+        <h2>{phone.brand} {phone.phone_name}</h2>
+        {/* Rest of the code */}
+      </div>
 
-//       <div className="details-specs">
-//         <h3>Specifications:</h3>
-//         {/* Rest of the code */}
-//       </div>
-//     </div>
-//   );
-// };
+      <div className="details-specs">
+        <h3>Specifications:</h3>
+        {/* Rest of the code */}
+      </div>
+    </div>
+  );
+};
 
-// export default Details;
+export default Details;
 
 
 
