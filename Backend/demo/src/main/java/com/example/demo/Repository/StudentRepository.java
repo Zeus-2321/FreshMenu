78ad80978efb3aec9ec948d7bf53e6be1bc4be53
chirag4582 <chirag.chavda@kalvium.community>
2023-07-17 07:04:32 +0530

    learnt spring security with jwt

--- Backend/demo/src/main/java/com/example/demo/Repository/StudentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.Repository;
-
-import com.example.demo.entity.StudentEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudentRepository extends JpaRepository<StudentEntity,Integer> {
-}
